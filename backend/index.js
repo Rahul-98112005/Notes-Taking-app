78f683d51bf588bb0e0841f5d6dc995eb6929d34
Rahul-98112005 <rahul98112005@gmail.com>
2025-01-07 19:19:10 +0530

    changes in get all user

--- backend/index.js
+++ backend/index.js
@@ -131,7 +131,7 @@ return res.sendStatus(401);
   }
 
     return res.json({
-   user: {fullName: isUser.fullNmae},
+   user: {fullName: isUser.fullName, email: isUser.email, "_id": isUser._id  , createdOn: isUser.createdOn},
    msg: ""
     });
 
